8f2596dde4dd19dda5fb144f39de5deb2fa6cfb6
Bhargav Kulkarni <coderbhargav8237@gmail.com>
2026-07-26 13:00:53 +0530

    feat : added user behavioral features

---
+++ backend/src/profiles/profile_manager.py
@@ -0,0 +1,13 @@
+from account_profile import AccountProfile
+
+class ProfileManager:
+
+    def __init__(self):
+        self.profiles = {}
+
+    def get_profile(self, account_id):
+
+        if account_id not in self.profiles:
+            self.profiles[account_id] = AccountProfile(account_id)
+
+        return self.profiles[account_id]
